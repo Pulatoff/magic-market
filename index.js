2751012607e7980396eea8805c06402285226b0a
Pulatoff <niyozbekpulatov@gmail.com>
2022-04-25 02:05:09 +0500

    products reliazed

--- index.js
+++ index.js
@@ -4,12 +4,32 @@ const { url } = require('inspector');
 const { add } = require('nodemon/lib/rules');
 
 const login = fs.readFileSync('./template/login.html','utf-8');
+const dataJson = fs.readFileSync('./data/data.json','utf-8')
+
+let dataAr = JSON.parse(dataJson)
+
+function renderHtml(html,obj){
+    let out = html;
+    out = out.replace('{imgProduct}',obj.image);
+    out = out.replace('{nameProduct}',obj.nameProduct);
+    out = out.replace('{priceProduct}',obj.price);
+    out = out.replace('{categoryProduct}',obj.category);
+    out = out.replace('{countSoldProduct}',obj.countSold);
+    return out
+}
 
 const server = http.createServer((req,res) =>{
     const urlcha = req.url
     if (urlcha === '/home'){
-        const home = fs.readFileSync('./template/home.html','utf-8');
+        const card = fs.readFileSync('./template/card.html','utf-8');
+        let home = fs.readFileSync('./template/home.html','utf-8');
         // home = home.replace('{urlImg}','');
+       const cardArr =  dataAr.map(val =>{
+         return   renderHtml(card,val)
+            
+        }).join('')
+        console.log(cardArr)
+        home = home.replace('{cardProduct}',cardArr)
         res.writeHead(200,{'Content-Type':'text/html'});
         res.end(home);
     }else if (urlcha === '/ownProdcuts'){
